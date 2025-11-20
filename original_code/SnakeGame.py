784c77b6f540a71d62d063acb2e0daf745324cae
alemuy <alemuy@berea.edu>
2025-11-19 23:42:40 -0500

    Class definition for SnakeGame

---
+++ original_code/SnakeGame.py
@@ -0,0 +1,31 @@
+class GameWindow:
+    def set_window_size(self):
+        pass  # Subtask I.A.1
+
+    def set_background_color(self):
+        pass  # Subtask I.A.2
+
+    def set_game_title(self):
+        pass  # Subtask I.A.3
+class Snake:
+    def set_starting_position(self):
+        pass  # Subtask I.B.1
+
+    def set_starting_length(self):
+        pass  # Subtask I.B.2
+
+    def set_initial_direction(self):
+        pass  # Subtask I.B.3
+class AppleManager:
+    def generate_apple_positions(self):
+        pass  # Subtask I.C.1
+
+    def assign_apple_types(self):
+        pass  # Subtask I.C.2
+class ScoreSystem:
+    def initialize_score(self):
+        pass  # Subtask I.D.1
+
+    def set_point_values(self):
+        pass  # Subtask I.D.2
+
