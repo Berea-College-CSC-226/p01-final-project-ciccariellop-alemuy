c63155c363ca0e9385a1344c4170568b00f6a170
alemuy <alemuy@berea.edu>
2025-11-19 23:46:15 -0500

    Class definition for SnakeGame

--- original_code/SnakeGame.py
+++ original_code/SnakeGame.py
@@ -28,4 +28,40 @@ class ScoreSystem:
 
     def set_point_values(self):
         pass  # Subtask I.D.2
+class InputHandler:
+    def detect_keyboard_input(self):
+        pass  # Subtask II.A
+
+    def interpret_arrow_keys(self):
+        pass  # Subtask II.B
+
+    def update_snake_direction(self):
+        pass  # Subtask II.C
+class CollisionSystem:
+    def check_apple_collision(self):
+        pass  # Subtask III.A
+
+    def check_wall_collision(self):
+        pass  # Subtask III.B
+
+    def check_self_collision(self):
+        pass  # Subtask III.C
+class AppleEffects:
+    def determine_apple_type(self):
+        pass  # Subtask IV.A
+
+    def apply_effect(self):
+        pass  # Subtask IV.B
+
+    def increase_length(self):
+        pass  # Subtask IV.B.1
+
+    def apply_special_effect(self):
+        pass  # Subtask IV.B.2
+
+    def remove_eaten_apple(self):
+        pass  # Subtask IV.B.3
+
+    def spawn_new_apple(self):
+        pass  # Subtask IV.C
 
